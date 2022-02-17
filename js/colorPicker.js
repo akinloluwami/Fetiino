95959a136002c8a380663e6cc78274a8a7b47482
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-02-17 14:08:45 +0100

    added copy to clipboard feature

--- js/colorPicker.js
+++ js/colorPicker.js
@@ -6,6 +6,7 @@ const rgbValue = document.querySelector(".rgb input");
 const cmykValue = document.querySelector(".cmyk input");
 const hslValue = document.querySelector(".hsl input");
 const hexValue = document.querySelector(".hex input");
+const resultsCard = document.querySelectorAll(".result_card");
 
 colorPicker.on("color:change", function (color) {
   const { hexString, rgbString, hslString } = color;
@@ -14,4 +15,20 @@ colorPicker.on("color:change", function (color) {
   hslValue.value = hslString;
   const getCodes = w3color(hexString);
   cmykValue.value = getCodes.toCmykString();
+});
+
+resultsCard.forEach((card) => {
+  const colorCodeValue = card.childNodes[3];
+  const copyButton = card.childNodes[5];
+  tippy(copyButton, {
+    content: "Copy",
+    animation: "fade",
+    followCursor: true,
+    hideOnClick: true,
+  });
+  copyButton.addEventListener("click", () => {
+    colorCodeValue.select();
+    colorCodeValue.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(colorCodeValue.value);
+  });
 });
