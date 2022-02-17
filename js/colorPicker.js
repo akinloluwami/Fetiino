1c7988d32af35ff7a4853f9461bc87004d03e645
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-02-17 14:05:40 +0100

    fixed issue with the cmyk input

--- js/colorPicker.js
+++ js/colorPicker.js
@@ -8,26 +8,10 @@ const hslValue = document.querySelector(".hsl input");
 const hexValue = document.querySelector(".hex input");
 
 colorPicker.on("color:change", function (color) {
-  const { hexString, rgbString, cmykString, hslString } = color;
+  const { hexString, rgbString, hslString } = color;
   hexValue.value = hexString;
   rgbValue.value = rgbString;
-  cmykValue.value = cmykString;
   hslValue.value = hslString;
-  function RgbToCmyk(R, G, B) {
-    if (R == 0 && G == 0 && B == 0) {
-      return [0, 0, 0, 1];
-    } else {
-      var calcR = 1 - R / 255,
-        calcG = 1 - G / 255,
-        calcB = 1 - B / 255;
-
-      var K = Math.min(calcR, Math.min(calcG, calcB)),
-        C = (calcR - K) / (1 - K),
-        M = (calcG - K) / (1 - K),
-        Y = (calcB - K) / (1 - K);
-
-      return [C, M, Y, K];
-    }
-  }
-  cmykValue.value = RgbToCmyk(color.rgb.r, color.rgb.g, color.rgb.b);
+  const getCodes = w3color(hexString);
+  cmykValue.value = getCodes.toCmykString();
 });
